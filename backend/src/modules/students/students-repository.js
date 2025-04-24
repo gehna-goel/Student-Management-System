d8896e447f5c8d878ed7b62ee5b847d15a2dffbe
gehna-goel <gehna.goell@gmail.com>
2025-04-23 20:37:19 -0700

    Update students-repository.js

--- backend/src/modules/students/students-repository.js
+++ backend/src/modules/students/students-repository.js
@@ -111,11 +111,19 @@ const findStudentToUpdate = async (paylaod) => {
     return rows;
 }
 
+const deleteStudentById = async (id) => {
+  const query = `DELETE FROM users WHERE id = $1`;
+  const queryParams = [id];
+  const { rowCount } = await processDBRequest({ query, queryParams });
+  return rowCount > 0;
+};
+
 module.exports = {
     getRoleId,
     findAllStudents,
     addOrUpdateStudent,
     findStudentDetail,
     findStudentToSetStatus,
-    findStudentToUpdate
+    findStudentToUpdate,
+    deleteStudentById
 };
